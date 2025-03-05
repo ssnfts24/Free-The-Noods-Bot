b8ecd8a3c8df0d6ecb2ad8d18dbff0f607309ecb
ssnfts24 <l.aaronpaul24@gmail.com>
2025-03-05 07:22:03 -0800

    Create events/guildMemberAdd.js

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'guildMemberAdd',
+  async execute(member) {
+    const welcomeChannelId = 'YOUR_WELCOME_CHANNEL_ID'; // Replace with your channel ID
+    const channel = member.guild.channels.cache.get(welcomeChannelId);
+    if (!channel) return;
+    const welcomeMessage = `Welcome to Free The Noods, ${member.user}! Enjoy your stay!`;
+    channel.send(welcomeMessage);
+  }
+};
